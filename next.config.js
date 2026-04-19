89f56e1c960e5418191bb07e6a07cb69cbc000b3
Satheeskumar Marikani <satheeskani1995@gmail.com>
2026-04-19 20:57:09 +0530

    fix: simplify next config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.optimization.runtimeChunk = false
-    }
-    return config
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
